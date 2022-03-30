73f5547ce6bd475e4c26ac7fd657a9fb9c7b5ac8
wcb1580 <100547447+wcb1580@users.noreply.github.com>
2022-03-30 23:16:37 +0800

    Update sdlab_earthquakes.py

--- sdlab_earthquakes.py
+++ sdlab_earthquakes.py
@@ -1,19 +1,3 @@
-# ENGSCI233: Lab - Sampled Data
-# sdlab_earthquakes.py
-
-# PURPOSE:
-# To INVESTIGATE a dataset using interpolation and integration methods.
-
-# PREPARATION:
-# Complete the activities in sdlab_practice.py.
-
-# SUBMISSION:
-# - YOU MUST submit a plot of NET MASS CHANGE as a function of time (sdlab_earthquakes.png)
-# - You MUST submit this file to complete the lab.
-
-
-# EXERCISE: Analysis of Net Mass Changes.
-#
 # Earthquakes are sometimes associated with oil and gas production (taking mass out of the 
 # ground) and injection (putting it back in) operations.
 # 
@@ -29,30 +13,6 @@
 # Although data from the two production wells (mass extractors) - PW1 and PW2 - are reported regularly,
 # data reporting from the injection well, IW1, is more irregular. In addition, the operator only 
 # reports MASS RATES, not CUMULATIVE production or injection MASS.
-#
-# TO solve this problem, you will need to use both INTERPOLATION and INTEGRATION.
-
-
-# TO DO:
-# - In sdlab_functions.py, COMPLETE the functions SPLINE_COEFFICIENT_MATRIX, SPLINE_RHS, and
-#   SPLINE_INTERPOLATE.
-# - Write a Newton-Cotes integration function OR find and use a built-in Python function.
-# - Produce a plot of NET MASS CHANGE as a function of time.
-# - ANSWER the questions in sdlab_questions.txt
-
-
-# HINTS:
-# - To add or difference two quantities, they should be measured or interpolated at the same time.
-# - You should consider a sensible strategy for the event that an interpolation point lies outside 
-#   the range of data (extrapolation).
-# - MASS RATE is a derivative (per unit time) and CUMULATIVE MASS is its integral.
-# - You will be assessed on the ability of your figure to convey information. Things that help:
-#    o Sensible labels (and units) for the axes.
-#    o A legend.
-#    o Sensible use of lines versus markers.
-#    o Juxtaposition of information in a way that highlights correlation.
-# - The plotting exercise in PRACTICE TASK ONE is relevant here.
-
 import numpy as np
 from numpy.linalg import norm, solve
 
